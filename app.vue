db1eed29e29384bda72c542c780bf0bf243839d3
Daniel Silva <daniel.silva@cegal.com>
2023-10-03 16:50:19 +0200

    Made nav sticky

--- app.vue
+++ app.vue
@@ -5,7 +5,9 @@
   <div class="min-h-screen">
     <Header />
     <div class="flex">
-      <div class="flex-auto w-3/12"><Navigation /></div>
+      <div class="flex-auto w-3/12 sticky top-6 self-start h-0">
+        <Navigation />
+      </div>
       <div class="flex-auto w-6/12"><NuxtPage /></div>
       <div class="flex-auto w-3/12"></div>
     </div>
