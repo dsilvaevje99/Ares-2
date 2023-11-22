7e621fd601336662707ac5c136b2f77cc4d73dd6
Daniel Silva <daniel.silva@cegal.com>
2023-11-22 13:12:33 +0100

    Only import and register display components

--- plugins/global-page-tiles.ts
+++ plugins/global-page-tiles.ts
@@ -1,7 +1,10 @@
 import { defineNuxtPlugin } from "#app";
-import * as components from "vue-page-tiles";
+import { PageTiles } from "vue-page-tiles";
+import * as components from "vue-page-tiles/display";
 
 export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.component("PageTiles", PageTiles);
+
   Object.entries(components).forEach(([name, component]) => {
     nuxtApp.vueApp.component(name, component);
   });
